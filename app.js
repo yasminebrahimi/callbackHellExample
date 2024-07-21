e1df960118d68851a955ce70ee7d9ab7fae15bc5
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2024-07-21 23:13:32 +0300

    small changes

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ function loginUser(email, password, callback) {
     setTimeout(() => {
         // Simulating a successful login by passing user data to the callback
         callback({ email });
-    }, 2000); // Simulating a 2-second delay
+    }, 3000); // Simulating a 2-second delay
 }
 
 // Function to simulate fetching enrolled courses with a delay
 function findEnrolledCourses(email, callback) {
     setTimeout(() => {
         // Simulating fetching enrolled courses for the given email
-        callback(["React.js", "Next.js", "Node.js"]);
-    }, 1000); // Simulating a 1-second delay
+        callback(["Java", "Next.js", "Node.js"]);
+    }, 2000); 
 }
 
 // Function to simulate fetching episodes of a course with a delay
@@ -29,7 +29,7 @@ function getEpisodes(courseTitle, callback) {
     setTimeout(() => {
         // Simulating fetching episodes for the given course title
         callback(["E01", "E02", "E03"]);
-    }, 2000); // Simulating a 2-second delay
+    }, 2000);
 }
 
 // Example usage of the loginUser function with nested callbacks
@@ -38,11 +38,11 @@ loginUser("user@gmail.com", "1234", (userData) => {
 
     // Nested callback to fetch enrolled courses once logged in
     findEnrolledCourses(userData.email, (courses) => {
-        console.log(courses); // Logging the enrolled courses
+        console.log(courses); 
 
         // Nested callback to fetch episodes of the first course
         getEpisodes(courses[0], (episodes) => {
-            console.log(episodes); // Logging the episodes of the course
+            console.log(episodes); 
         });
     });
 });
